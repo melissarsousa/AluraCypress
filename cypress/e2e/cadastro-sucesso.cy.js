d2b7b606295c0a02f68d0b514c0bbde466458cd7
Melissa Rios <melissa.rios.sousa@gmail.com>
2024-10-07 17:48:30 -0300

    untrack files contained in the .gitignore file

--- cypress/e2e/cadastro-sucesso.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Página de Cadastro com sucesso', () => {
-  it('Cadastro com dados válidos ', () => {
-    cy.visit('https://adopet-frontend-cypress.vercel.app/');
-    cy.cadastrar('Bsinha Bsa','bsabsa@email.com','Bsabsinha7')
-  })
-})
